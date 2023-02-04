1cb9e6b64ab6a3ee52ade8be90027a7ad1eb39fb
Ilona Sellenberk <ilona.sell@gmail.com>
2023-02-04 21:39:43 +0100

    ContentView clear from code into Barcode View with input from user

--- PassionFood/Views/ContentView.swift
+++ PassionFood/Views/ContentView.swift
@@ -7,36 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @StateObject var infoViewModel = InfoTableViewModel()
-    
+struct ContentView: View {    
     var body: some View {
         VStack {
-            // Button("Get Info", action: infoVM.getInfo)
-            Spacer()
-            
-            // Make shorter variable called infoProduct
-            if let data = infoViewModel.infoTable {
-                // AsyncImage(url: URL(string: (infoViewModel.infoTable?.product.image_url)!))
-                AsyncImage(url: URL(string: (data.product.image_url)))
-                    .scaledToFit()
-            } else {
-                Text("No Image yet...")
-            }
-            
-            Button {
-                Task {
-                    await infoViewModel.getInfo()
-    // uses 'async' keyword in the method in the VM, here View 'await'
-                }
-            } label: {
-                Text("Just One Click")
-                    .frame(maxWidth: 200)
-                    .foregroundColor(.gray)
-                    .background(.yellow)
-                    .padding(30)
-            }
-            
+            BarcodeView()
         }
     }
 }
