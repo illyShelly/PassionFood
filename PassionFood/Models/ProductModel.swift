36bc045aefd9f1c8565c3612f7f55723981d7565
Ilona Sellenberk <ilona.sell@gmail.com>
2023-02-06 10:42:13 +0100

    create ProductModel new structure for further use

--- PassionFood/Models/ProductModel.swift
+++ PassionFood/Models/ProductModel.swift
@@ -1,8 +1,23 @@
 //
-//  Product.swift
+//  ProductModel.swift
 //  PassionFood
 //
 //  Created by Ilona Sellenberkova on 05/02/2023.
 //
 
 import Foundation
+
+struct ProductModel {
+    let name: String
+    let brand: String
+    let image: String
+    let keywords: [String]
+    let additives: [String]
+    let nutriments: Nutriment
+}
+
+
+//struct Nutriment: {
+//    let carbohydrates: Double
+//    let energy: Double
+//}
