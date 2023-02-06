1f4121c9b826b9b0f373f3b119c88212906494ad
Ilona Sellenberk <ilona.sell@gmail.com>
2023-02-06 10:44:11 +0100

    update ProductView with Nutriments

--- PassionFood/Views/ProductView.swift
+++ PassionFood/Views/ProductView.swift
@@ -10,16 +10,48 @@ import SwiftUI
 struct ProductView: View {
     @ObservedObject var infoVM: InfoTableViewModel
     @Binding var productFound: Bool
-    
+        
     var body: some View {
-        ScrollView {
-            if let _ = infoVM.infoTable {  // a.
-                    AsyncImage(url: URL(string: (infoVM.infoTable?.product?.image_url)!))
-                        .scaledToFit()
+//        ScrollView {
+            VStack {  // alignment: .center, spacing: 20
+                if let product = infoVM.infoTable?.product {  // a.
+                    HStack {
+                        AsyncImage(url: URL(string: (product.image_url)))
+                                    .scaledToFit()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 80, height: 80)
+                    }
+
+                    Text(product.product_name)
+                    Text(product.brands)
+                    Spacer()
+                    if let arrGlutenInfo = product._keywords {
+                        ForEach(arrGlutenInfo, id: \.self) { item in
+                            VStack {
+                                Text("Keywords: \(item)")
+                            }
+                        }
+                    }
+
+                    if let nutriments = infoVM.infoTable?.nutriments {
+                        VStack {
+                            Text("Energy \(String(format: "%.0f", nutriments.energy))")
+                            Text("Carbo \(String(format: "%.1f", nutriments.carbohydrates))")
+                            Text("Fat \(String(format: "%.1f", nutriments.fat))")
+                            Text("Protein \(String(format: "%.1f", nutriments.proteins))")
+                            Text("Salt \(String(format: "%.1f", nutriments.salt))")
+                        }
+                    }
+
+                    // Display all additives if any
+                    if var _ = product.additives_tags {
+                        AdditivesView(infoVM: infoVM)
+                    }
+                }
+                
             }
+              
         }
-      
-    }
 }
 
 struct ProductView_Previews: PreviewProvider {
